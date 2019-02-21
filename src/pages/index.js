fe5b7435a6cffe26ab1fbcac8fd7ca7f4b2134f0
alexandrtovmach <alexandrtovmach@gmail.com>
2019-02-21 18:56:29 +0200

    added links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <ul>
+      <li><Link to="/about">about</Link></li>
+      <li><Link to="/tutorial/part-zero">Part #0</Link></li>
+      <li><Link to="/tutorial/part-one">Part #1</Link></li>
+      <li><Link to="/tutorial/part-two">Part #2</Link></li>
+      <li><Link to="/tutorial/part-three">Part #3</Link></li>
+      <li><Link to="/tutorial/part-four">Part #4</Link></li>
+    </ul>
+  </div>
+)
