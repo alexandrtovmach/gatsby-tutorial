f067b5157502923142eba3a024226a4533596d4e
alexandrtovmach <alexandrtovmach@gmail.com>
2019-02-21 20:32:43 +0200

    created header & sidebar components

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import logoSrc from "../images/logo.png"
+
+export default () => (
+  <header>
+    <img src={logoSrc} alt="logo" width="60px" height="60px" />
+    That's header
+  </header>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
+
+import Header from "../components/header";
+import Sidebar from "../components/sidebar";
 
 export default () => (
   <div>
-    <ul>
-      <li><Link to="/about">about</Link></li>
-      <li><Link to="/tutorial/part-zero">Part #0</Link></li>
-      <li><Link to="/tutorial/part-one">Part #1</Link></li>
-      <li><Link to="/tutorial/part-two">Part #2</Link></li>
-      <li><Link to="/tutorial/part-three">Part #3</Link></li>
-      <li><Link to="/tutorial/part-four">Part #4</Link></li>
-    </ul>
+    <Header />
+    <Sidebar />
+    <h1>Index page</h1>
   </div>
 )
