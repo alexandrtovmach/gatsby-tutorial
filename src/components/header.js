2ecd1384087bc122547bfb8472e4c5c4d6dce324
alexandrtovmach <alexandrtovmach@gmail.com>
2019-02-22 21:10:54 +0200

    import logo with GraphQL

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import logoSrc from "../images/logo.png"
+import { Link, StaticQuery, graphql } from "gatsby"
 
 export default () => (
-  <header>
-    <Link to="/">
-      <img src={logoSrc} alt="logo" width="60px" height="60px" />
-    </Link>
-    That's header
-  </header>
+  <StaticQuery
+    query={graphql`
+      {
+        allFile(filter: { name: { eq: "logo" } }) {
+          edges {
+            node {
+              publicURL
+            }
+          }
+        }
+      }
+    `}
+    render={({
+      allFile: {
+        edges: [{
+          node: {
+            publicURL
+          }
+        }]
+      }
+    }) => (
+      <header>
+        <Link to="/">
+          <img src={publicURL} alt="logo" width="60px" height="60px" />
+        </Link>
+        That's header
+      </header>
+    )}
+  />
 )
